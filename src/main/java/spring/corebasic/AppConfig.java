1b87171cb0ee7c0acbf13cdbaa3c2ac7890dc283
Hanna <hanna@trdst.com>
2023-07-24 15:23:03 +0900

    refactor: 역할에 따른 구현이 잘 보이도록 AppConfig 리팩터링

--- src/main/java/spring/corebasic/AppConfig.java
+++ src/main/java/spring/corebasic/AppConfig.java
@@ -1,6 +1,8 @@
 package spring.corebasic;
 
+import spring.corebasic.discount.DiscountPolicy;
 import spring.corebasic.discount.FixDiscountPolicy;
+import spring.corebasic.member.MemberRepository;
 import spring.corebasic.member.MemberService;
 import spring.corebasic.member.MemberServiceImpl;
 import spring.corebasic.member.MemoryMemberRepository;
@@ -10,11 +12,19 @@ import spring.corebasic.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
