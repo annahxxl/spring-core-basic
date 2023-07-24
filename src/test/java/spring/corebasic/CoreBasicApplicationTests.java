0cc22094bcdcc238779cd235aaee179cb1600d11
Hanna <hanna@trdst.com>
2023-07-24 12:42:50 +0900

    first commit

---
+++ src/test/java/spring/corebasic/CoreBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.corebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
