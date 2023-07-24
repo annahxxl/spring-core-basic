a03792ac9012e2a460e721cb56b0a4a6c7a491b0
Hanna <hanna@trdst.com>
2023-07-24 14:55:42 +0900

    feat: 새로운 할인 정책 적용

--- src/main/java/spring/corebasic/order/OrderServiceImpl.java
+++ src/main/java/spring/corebasic/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package spring.corebasic.order;
 
 import spring.corebasic.discount.DiscountPolicy;
 import spring.corebasic.discount.FixDiscountPolicy;
+import spring.corebasic.discount.RateDiscountPolicy;
 import spring.corebasic.member.Member;
 import spring.corebasic.member.MemberRepository;
 import spring.corebasic.member.MemoryMemberRepository;
@@ -9,7 +10,8 @@ import spring.corebasic.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
